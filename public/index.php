2313111792df920820d644336e7b4357a5f859d6
Easton Elliott <easton@geekness.eu>
2018-03-04 15:20:20 -0800

    Update radius to 115m, code cleanup

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+require '../settings.php';
+
 //Config to display errors or not
 if($config['debug_mode'] == TRUE){
    if(rmdir('./cache') == FALSE){
@@ -11,7 +13,7 @@ if($config['debug_mode'] == TRUE){
 }
 
 
-require '../settings.php';
+
 require '../vendor/autoload.php';
 require '../Sms_job.php';
 
@@ -56,38 +58,27 @@ if(!empty($_GET['lat']) && !empty($_GET['long'])){
 
   $geo['long'] = substr($_GET['long'], 0, 10);
   //First we get the bus stops near our location                 
-  $stops   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops?apikey='.$config['apikey'].'&lat='.$geo['lat'].'&long='.$geo['long'].'&radius=100', false, $context);
+  $stops   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops?apikey='.$config['apikey'].'&lat='.$geo['lat'].'&long='.$geo['long'].'&radius=115', false, $context);
   $stops_output      = json_decode($stops);
+
 
   //If we get back failure from the HTTP request
   if($stops == FALSE){
-    $notice = 'No stops near you within 100m, sorry! Try with an address instead';
+    $notice = 'No stops near you within 115m, sorry! Try with an address instead. ';
     error_log('no stops near lat '.$geo['lat'].' long '.$geo['long'], 0);
+    error_log($http_response_header[0], 0);
+
+    if($http_response_header[0] == 'HTTP/1.1 404 Not Found'){
+      $notice .= "BUS 404 NOT FOUND";
+      error_log('No stops near '.$geo['lat'].'& '.$geo['long'], 0);
+    }
 
   }else{
 
-error_log($stops_output, 0);
-  $bus_time   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops/'.$stops_output[0]->StopNo.'/estimates?apikey='.$config['apikey'].'&count=2&timeframe=65', false, $context);
-  $bus_time_output      = json_decode($bus_time);
+        $bus_time   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops/'.$stops_output[0]->StopNo.'/estimates?apikey='.$config['apikey'].'&count=2&timeframe=65', false, $context);
+        $bus_time_output      = json_decode($bus_time);
 
-
-
-switch($bus_time_output[0]->Schedules[0]->ScheduleStatus){
-     case "*":
-          $bus_schedule = "On time";
-          break;
-
-     case "-":
-          $bus_schedule = "Delay";
-          break;
-
-     case "+":
-          $bus_schedule = "Fast";
-          break;          
-}
-
-
-}
+      }
 }
 
 if(isset($_GET['address'])){
@@ -96,7 +87,7 @@ if(isset($_GET['address'])){
   $output      = json_decode($geocode);
 
   if($output->status == "ZERO_RESULTS"){
-    echo 'No results for address';
+    $notice = 'No results for address';
     error_log("No results for address: $address", 0);
   }
 
@@ -120,19 +111,7 @@ if(isset($_GET['address'])){
 
 
 
-        switch($bus_time_output[0]->Schedules[0]->ScheduleStatus){
-             case "*":
-                  $bus_schedule = "On time";
-                  break;
 
-             case "-":
-                  $bus_schedule = "Delay";
-                  break;
-
-             case "+":
-                  $bus_schedule = "Fast";
-                  break;          
-              }
     }
 }
 
@@ -140,30 +119,44 @@ if(isset($_GET['address'])){
 if(isset($_GET['stopno'])){
 
   $stopnumber = trim($_GET['stopno']);
-  $bus_time   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops/'.$stopnumber.'/estimates?apikey='.$config['apikey'].'&count=2&timeframe=95', false, $context);
+  $stopnumber = intval($stopnumber);
+
+  if(isset($_GET['routeno'])){
+      $route_number =trim($_GET['routeno']);
+      $route_number = intval($route_number);
+      $route_number = '&RouteNo='.$route_number;
+  }else{
+      $route_number = null;
+  }
+
+  $bus_time   = file_get_contents('http://api.translink.ca/rttiapi/v1/stops/'.$stopnumber.'/estimates?apikey='.$config['apikey'].'&count=2&timeframe=95'.$route_number, false, $context);
 
   $bus_time_output      = json_decode($bus_time);
 
   if($bus_time_output == NULL){
     $notice .=  "Not a valid bus stop number or it's too early/late!";
   }
-  switch($bus_time_output[0]->Schedules[0]->ScheduleStatus){
+}
+
+$a = count($bus_time_output);
+error_log($a, 0);
+switch($bus_time_output[0]->Schedules[0]->ScheduleStatus){
      case "*":
-          $bus_schedule = "On time";
+          $bus_schedule = "on time";
           break;
 
      case "-":
-          $bus_schedule = "Delay";
+          $bus_schedule = "delay 🐢";
           break;
 
      case "+":
-          $bus_schedule = "Fast";
-          break;          
+          $bus_schedule = "fast";
+          break; 
 
-
-}
-}
-
+      default:
+          $bus_schedule = null;
+          break;            
+      }
 
 echo $twig->render('index.twig', array('closest_stop' => $stops_output[0]->StopNo,
     'stop_location' => $stops_output[0]->Name,
@@ -171,6 +164,6 @@ echo $twig->render('index.twig', array('closest_stop' => $stops_output[0]->StopN
     'next_estimate' => $bus_time_output[0]->Schedules,
     'bus_schedule_time' => $bus_schedule,
     'dest_phone_number' => $_COOKIE['dest_phone_number'],
+    'bus_time_output' => $bus_time_output,
+    'stop_number' => $stopnumber,
     'notice' => $notice));
-
-
--- public/response.php
+++ public/response.php
@@ -12,11 +12,27 @@ require '../Sms_job.php';
 Resque::setBackend($settings['REDIS_BACKEND']);
 
 
-  $minute = $_POST['minute'];
-  $countdown = $_POST['countdown'];
+  $minute = intval($_POST['minute']);
+  $countdown = intval($_POST['countdown']);
   $route_no = $_POST['route_no'];
+  
+
   $remind_time = $countdown - $minute;
+if(preg_match("/^[0-9]{3}[0-9]{3}[0-9]{4}$/", $_POST['phonenumber'])) {
+  // $phone is valid
   $phone_number = $_POST['phonenumber'];
+}else{
+
+  $notice = "Invalid phone number";
+  $return = array('success' => 0, 'response' => $notice);
+
+  echo json_encode($return);
+
+  error_log(json_encode($return), 0);
+  error_log('Invalid phone number!', 0);
+
+  die();
+}
 
 //If the cookie phone number isn't set, set it now
 if(!isset($_COOKIE['dest_phone_number'])) {
@@ -31,13 +47,19 @@ $args = array(
         );
 
 $seconds = $remind_time * 60;
+
 //Schedule the SMS
+//
+//Seconds to send, queue ID, PHP class, data to send
 ResqueScheduler::enqueueIn($seconds, 'test', 'Sms_job', $args);
 
 $notice = "Sending in ".$remind_time." minutes!";
 $return = array('success' => 1, 'response' => $notice);
 
 echo json_encode($return);
+error_log('SMS sent successfully!', 0);
 }else{
+  error_log('Error on SMS sending', 0);
   die('cant!');
+
 }
